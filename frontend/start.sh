4a92ee53b0bd8524ae452546301bdf79f09707b3
Anastasia <grialtt@gmail.com>
2023-07-23 20:54:38 +0200

    Update to start.sh file

--- frontend/start.sh
+++ frontend/start.sh
@@ -3,23 +3,21 @@
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
-CONTRACT_DIRECTORY=../contract
-DEV_ACCOUNT_FILE="${CONTRACT_DIRECTORY}/neardev/dev-account.env"
-
 start () {
   echo The app is starting!
-  env-cmd -f $DEV_ACCOUNT_FILE parcel index.html --open
+  parcel index.html --open
 }
 
 alert () {
   echo "======================================================"
-  echo "It looks like you forgot to deploy your contract"
-  echo ">> Run ${GREEN}'npm run deploy'${NC} from the 'root' directory"
+  echo "It looks like you forgot to set your environment variables"
+  echo ">> Set the environment variables in the CI/CD settings"
   echo "======================================================"
 }
 
-if [ -f "$DEV_ACCOUNT_FILE" ]; then
-  start
-else
+# Check if the required environment variables are set
+if [ -z "$CONTRACT_NAME" ]; then
   alert
+else
+  start
 fi
